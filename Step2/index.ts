0cf875f2feccb9e4648b22d0f8c9b3bb7714d799
Dominik Tornow <dominik.tornow@gmail.com>
2024-03-19 14:07:27 -0700

    Update index.ts
    Add start

--- Step2/index.ts
+++ Step2/index.ts
@@ -5,21 +5,28 @@ import { downloadAndSummarize } from "./app";
 // Initialize a Resonate application.
 const resonate = new Resonate({ url: "http://localhost:8001" });
 
+// Register a function as a Resonate function
 resonate.register("downloadAndSummarize", downloadAndSummarize, resonate.options({ timeout: Number.MAX_SAFE_INTEGER }));
 
-const app = express();
+// Start the Resonate application
+resonate.start();
 
-app.use(express.json())
+// Initialize an Express application.
+const app = express().use(express.json());
 
+// Register a function as an Express endpoint
 app.post("/summarize", async (req: Request, res: Response) => {
     const url = req.body?.url;
     try {
-        res.send(await resonate.run("downloadAndSummarize", url, url))
+        // Call the resonate function
+        let summary = await resonate.run("downloadAndSummarize", /* id */ `summarize-${url}`, /* param */ url);
+        res.send(summary);
     } catch (e) {
-        res.status(500);
+        res.status(500).send("An error occurred.");
     }
 });
 
+// Start the Express application
 app.listen(3000, () => {
     console.log("Listening on port 3000");
 });
